6957aa6b1049d7250533b2394336e000dfe6df8d
Kevin Allen <kevin@intelliware.ca>
2015-05-13 11:59:32 -0400

    Added java and a test

---
+++ src/main/java/com/koatest/util/CollectionUtils.java
@@ -0,0 +1,8 @@
+package com.koatest.util;
+
+/**
+ * Created by allenk on 13/05/2015.
+ */
+public class CollectionUtils {
+
+}
---
+++ src/test/java/com/koatest/util/CollectionsUtilsTest.java
@@ -0,0 +1,16 @@
+package com.koatest.util;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by allenk on 13/05/2015.
+ */
+public class CollectionsUtilsTest {
+
+
+    @Test
+    public void testString() {
+        Assert.assertNotNull("not null");
+    }
+}
